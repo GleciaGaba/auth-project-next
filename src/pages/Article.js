d92586e21d6b15a0b53e56d7ab3f40b82fa74cf7
GleciaGaba <gleciasf@hotmail.com>
2024-11-25 21:45:51 +0100

    update

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -26,6 +26,7 @@ export default function Article() {
   const [newTitle, setNewTitle] = useState("");
   const [newDescription, setNewDescription] = useState("");
   const [error, setError] = useState(null);
+  const [editCommentId, setEditCommentId] = useState(null); // État pour suivre le commentaire en mode édition
 
   useEffect(() => {
     const fetchComments = async () => {
@@ -37,7 +38,7 @@ export default function Article() {
           console.error("Failed to decode token");
           return;
         }
-        const userId = decodedToken.userId;
+        const userId = decodedToken.userId; // Assurez-vous que le token contient un champ userId
         console.log("User ID:", userId);
 
         try {
@@ -46,7 +47,7 @@ export default function Article() {
             {
               method: "GET",
               headers: {
-                Authorization: `Bearer ${token}`,
+                Authorization: `Bearer ${token}`, // Le backend utilise ce token pour extraire userId
               },
             }
           );
@@ -58,7 +59,7 @@ export default function Article() {
           }
 
           const data = await response.json();
-          console.log("Fetched data:", data);
+          console.log("Fetched data:", data); // Ajoutez ce journal pour vérifier les données reçues
           if (Array.isArray(data.data)) {
             const userComments = data.data.filter(
               (comment) => comment.userId._id === userId
@@ -114,49 +115,57 @@ export default function Article() {
   };
 
   const handleUpdate = async (commentId, updatedTitle, updatedDescription) => {
-    try {
-      console.log(
-        "Updating comment:",
-        commentId,
-        updatedTitle,
-        updatedDescription
-      );
-      const response = await fetch(
-        `http://localhost:3001/api/posts/update-post/${commentId}`,
-        {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-          body: JSON.stringify({
-            title: updatedTitle,
-            description: updatedDescription,
-          }),
+    if (editCommentId === commentId) {
+      // Si le commentaire est déjà en mode édition, soumettre les modifications
+      try {
+        console.log(
+          "Updating comment:",
+          commentId,
+          updatedTitle,
+          updatedDescription
+        ); // Ajoutez ce journal pour vérifier les données envoyées
+        const response = await fetch(
+          `http://localhost:3001/api/posts/update-post?_id=${commentId}`, // Utilisez les paramètres de requête pour passer l'ID du commentaire
+          {
+            method: "PUT",
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: `Bearer ${localStorage.getItem("token")}`,
+            },
+            body: JSON.stringify({
+              title: updatedTitle,
+              description: updatedDescription,
+            }),
+          }
+        );
+
+        if (!response.ok) {
+          const errorText = await response.text();
+          console.error("Error response text:", errorText);
+          throw new Error("Network response was not ok");
         }
-      );
 
-      if (!response.ok) {
-        const errorText = await response.text();
-        console.error("Error response text:", errorText);
-        throw new Error("Network response was not ok");
+        const updatedComment = await response.json();
+        setComments((prevComments) =>
+          prevComments.map((comment) =>
+            comment._id === commentId ? updatedComment : comment
+          )
+        );
+        setEditCommentId(null); // Quitter le mode édition
+      } catch (error) {
+        console.error("Error updating comment:", error);
       }
-
-      const updatedComment = await response.json();
-      setComments((prevComments) =>
-        prevComments.map((comment) =>
-          comment._id === commentId ? updatedComment : comment
-        )
-      );
-    } catch (error) {
-      console.error("Error updating comment:", error);
+    } else {
+      // Passer en mode édition
+      setEditCommentId(commentId);
     }
   };
 
   const handleDelete = async (commentId) => {
     try {
+      console.log("Deleting comment:", commentId); // Ajoutez ce journal pour vérifier les données envoyées
       const response = await fetch(
-        `http://localhost:3001/api/posts/delete-post/${commentId}`,
+        `http://localhost:3001/api/posts/delete-post?_id=${commentId}`,
         {
           method: "DELETE",
           headers: {
@@ -193,7 +202,7 @@ export default function Article() {
       <Navbar />
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
         <div className="bg-white p-8 rounded shadow-md w-full max-w-2xl">
-          <h1 className="text-2xl mb-4">Comments</h1>
+          <h1 className="text-2xl mb-4">Article Comments</h1>
           <form onSubmit={handleSubmit} className="mb-4">
             <input
               value={newTitle}
@@ -224,24 +233,32 @@ export default function Article() {
                   key={index}
                   className="mb-4 p-4 border border-gray-300 rounded bg-gray-50 shadow-sm max-w-full"
                 >
-                  <input
-                    value={comment.title}
-                    onChange={(e) => handleInputChange(e, comment._id, "title")}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  />
-                  <textarea
-                    value={comment.description}
-                    onChange={(e) =>
-                      handleInputChange(e, comment._id, "description")
-                    }
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  />
+                  {editCommentId === comment._id ? (
+                    <>
+                      <input
+                        value={comment.title}
+                        onChange={(e) =>
+                          handleInputChange(e, comment._id, "title")
+                        }
+                        className="shadow appearance-none border rounded w-full py-2 px-3 mb-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      />
+                      <textarea
+                        value={comment.description}
+                        onChange={(e) =>
+                          handleInputChange(e, comment._id, "description")
+                        }
+                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <h2 className="text-xl font-bold">{comment.title}</h2>
+                      <p className="text-gray-700">{comment.description}</p>
+                    </>
+                  )}
                   <p>
                     Created at: {new Date(comment.createdAt).toLocaleString()}
                   </p>
-                  <button className="mr-2 p-2 bg-green-500 text-white rounded">
-                    Modify
-                  </button>
                   <button
                     onClick={() =>
                       handleUpdate(
@@ -252,7 +269,7 @@ export default function Article() {
                     }
                     className="mr-2 p-2 bg-yellow-500 text-white rounded"
                   >
-                    Update
+                    {editCommentId === comment._id ? "Submit" : "Update"}
                   </button>
                   <button
                     onClick={() => handleDelete(comment._id)}
